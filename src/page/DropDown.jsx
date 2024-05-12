5e322ceac76348ae0bce3043ece54c12ff2e8fe8
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-05-12 23:27:59 +0600

    Input part not perfactly work

--- src/page/DropDown.jsx
+++ src/page/DropDown.jsx
@@ -1,7 +1,9 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 
 const DropDown = () => {
     let drofRef = useRef(null)
+    let inputRef = useRef(null)
+    let [input, setInput] = useState("")
     let HandleClick = (e) => {
         if (drofRef.current.style.display == "none") {
             drofRef.current.style.display = "block"
@@ -11,18 +13,37 @@ const DropDown = () => {
             }
         }
     }
+    let handleInput = ()=>{
+        if (input === "") {
+            inputRef.current.focus()
+        }else{
+            inputRef.current.style.backgroundColor = "red"
+        }
+        
+    }
+    let handleChange = (e)=>{
+        setInput(e.target.value);
+    }
+    console.log(input);
   return (
-    <div onClick={HandleClick} >
-        <button  >Open</button>
-        <ul ref={drofRef} style={{display: "none", backgroundColor: "red"}}>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-            <li>Hello</li>
-        </ul>
-        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, repellat?</div>
-    </div>
+    <>
+        <div onClick={HandleClick} style={{height: "300px", backgroundColor: 'gray'}}>
+            <button  >Open</button>
+            <ul ref={drofRef} style={{display: "none", backgroundColor: "red"}}>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+            </ul>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, repellat?</div>
+        </div>
+        <div style={{height: "50px", backgroundColor: 'orange'}}>
+            <input onChange={handleChange} ref={inputRef} type="text" />
+            <button onClick={handleInput}>Click</button>
+            <h1>{input}</h1>
+        </div>
+    </>
   )
 }
 
