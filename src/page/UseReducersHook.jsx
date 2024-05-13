db26654ff085530692ee616e0a188bf49034048f
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-05-13 13:15:45 +0600

    useReducer Hook more practice

--- src/page/UseReducersHook.jsx
+++ src/page/UseReducersHook.jsx
@@ -16,8 +16,43 @@ function reducer(state, action) {
   }
 }
 
+// const initialTodos = [
+//   {
+//     id: 1,
+//     title: "Todo 1",
+//     complete: false,
+//   },
+//   {
+//     id: 2,
+//     title: "Todo 2",
+//     complete: false,
+//   },
+// ];
+
+// const reducer = (state, action) => {
+//   switch (action.type) {
+//     case "COMPLETE":
+//       return state.map((todo) => {
+//         if (todo.id === action.id) {
+//           return { ...todo, complete: !todo.complete };
+//         } else {
+//           return todo;
+//         }
+//       });
+//     default:
+//       return state;
+//   }
+// };
+
 const UseReducersHook = () => {
   const [state, dispatch] = useReducer(reducer, initialState)
+
+  // const [todos, dispatch] = useReducer(reducer, initialTodos);
+
+  // const handleComplete = (todo) => {
+  //   dispatch({ type: "COMPLETE", id: todo.id });
+    
+  // }
   return (
     <>
       <div style={{marginBottom: "20px", backgroundColor: "#FF5733"}}>
@@ -25,6 +60,21 @@ const UseReducersHook = () => {
         <button onClick={()=>dispatch({type: "plus"})}>Plus</button>
         <button onClick={()=>dispatch({type: "minus"})}>Minus</button>
       </div>
+
+      {/* <>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <label>
+            <input
+              type="checkbox"
+              checked={todo.complete}
+              onChange={() => handleComplete(todo)}
+            />
+            {todo.title}
+          </label>
+        </div>
+      ))}
+    </> */}
     </>
   )
 }
